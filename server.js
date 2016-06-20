2d1cd6e3546c690bc170e175c35a13be9819b670
Nuttdguy <pygnasak@gmail.com>
2016-06-20 12:55:16 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var url = require("url");
+var count = 0;
+
+function getProperty(re) {
+	var arr = [];
+
+	for (var i in re) {
+		if ( i === "url") {
+			arr.push(re[i]);
+		}
+	}
+	return arr;
+}
+
+function start(route) {
+	function onRequest(request, response) {
+		// console.log("Request received." + "== " + console.log(getProperty(request)) + count);
+
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received. " );
+
+		route(pathname);
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World! Does this change?");
+		count++;
+		response.end();
+
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;

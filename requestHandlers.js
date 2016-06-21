80ee9fc23c3b09196a299cae0bc954085264670d
Nuttdguy <pygnasak@gmail.com>
2016-06-21 00:00:32 -0500

    Add functionality for uploading file

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,7 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
 
 function start(response) {
 	console.log("Request handler 'Start' was called.")
@@ -9,26 +12,49 @@ function start(response) {
 		'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> '+
 		'</head>'+
 		'<body>'+
-		'<form action="/upload" method="post">'+
-		'<textarea name="text" rows="20" cols="60"></textarea>'+
-		'<input type="submit" value="Submit Text" />'+
+		'<form action="/upload" enctype="multipart/form-data" method="post">'+
+		'<input type="file" name="upload" multiple="multiple">'+
+		'<input type="submit" value="Upload file" />'+
 		'</form>'+
 		'</body>'+
 		'</html>';
+
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write(body);
 		response.end();
 }
 
-function upload(response) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.")
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Upload");
-	response.end();
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+
+		fs.rename(files.upload.path, "tmp/temp.jpg", function(error) {
+			if (error) {
+				fs.unlink("tmp/temp.jpg");
+				fs.rename(files.upload.path, "tmp/temp.jpg");
+			}
+		});
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("received image: <br/>");
+		response.write("<img src='/show' />")
+		response.end();
+	});
 }
+
+function show(response) {
+	console.log("Request handler 'show' was called.");
+	response.writeHead(200, {"Content-Type": "image/jpg"});
+	fs.createReadStream("tmp/temp.jpg").pipe(response);
+}
+
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
 
 // function start(response) {
